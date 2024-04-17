ce01b3dd8aa86c6ba13641db4104ec84d944af28
Fakhry224 <112813649+Fakhry224@users.noreply.github.com>
2024-04-17 14:09:13 +0700

    Update database.sql

--- TugasProject1-PEMWEB/database.sql
+++ TugasProject1-PEMWEB/database.sql
@@ -1,7 +1,17 @@
-CREATE TABLE user (
+-- Create database
+CREATE DATABASE IF NOT EXISTS projek_pemweb;
+
+-- Use database
+USE projek_pemweb;
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE users (
     user_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     email VARCHAR(60) NOT NULL,
     hp VARCHAR(13) NOT NULL,
     alamat VARCHAR(255) NOT NULL,
@@ -9,12 +19,26 @@ CREATE TABLE user (
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE product (
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE products (
     product_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    description TEXT,
+    description TEXT NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock INT UNSIGNED NOT NULL,
     file_path VARCHAR(255) NOT NULL,
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+-- 
+-- Relasi antar produk dan user
+--
+
+ALTER TABLE products
+ADD COLUMN user_id INT(6) UNSIGNED,
+ADD CONSTRAINT fk_user_id
+FOREIGN KEY (user_id)
+REFERENCES users(user_id);
