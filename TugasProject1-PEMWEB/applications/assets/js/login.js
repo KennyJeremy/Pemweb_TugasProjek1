ec61f60b67c54d391f7fba6559612edb724c77d9
Fakhry224 <112813649+Fakhry224@users.noreply.github.com>
2024-04-24 11:00:33 +0700

    Update login.js

--- TugasProject1-PEMWEB/applications/assets/js/login.js
+++ TugasProject1-PEMWEB/applications/assets/js/login.js
@@ -2,6 +2,14 @@ function submitLoginForm() {
   const formData = new FormData(document.getElementById("loginForm"));
   const url = "../controllers/login.php";
 
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  if (email === "" || password === "") {
+    alert("Mohon lengkapi semua field!");
+    return;
+  }
+  
   axios
     .post(url, formData)
     .then((res) => {
